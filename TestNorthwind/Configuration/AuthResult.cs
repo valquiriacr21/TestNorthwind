39fb8427d91681bc036c2f1e67776d5b48a689c6
Loyda <valquiriacr21@gmail.com>
2022-07-22 00:38:36 -0400

    Configured AuthResult

---
+++ TestNorthwind/Configuration/AuthResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TestNorthwind.Configuration
+{
+    public class AuthResult
+    {
+        public string Token { get; set; }
+        public bool Succes { get; set; }
+
+        public List<string> Errors { get; set; } 
+
+    }
+}
